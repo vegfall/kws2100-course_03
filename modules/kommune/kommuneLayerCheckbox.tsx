12af7370dc139557516bfecb99e63500a9f228f3
Vegard Falldalen <vegfall@gmail.com>
2024-01-31 12:43:31 +0100

    Clicking kommune shows name

--- modules/kommune/kommuneLayerCheckbox.tsx
+++ modules/kommune/kommuneLayerCheckbox.tsx
@@ -3,26 +3,56 @@ import { Layer } from "ol/layer";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
+import { Feature, Map, MapBrowserEvent } from "ol";
+import { Polygon } from "ol/geom";
 
+type KommuneProperties = {
+  kommunenummer: string;
+  navn: {
+    sprak: string;
+    navn: string;
+  }[];
+};
+
+type KommuneFeatures = Feature<Polygon> & {
+  getProperties(): KommuneProperties;
+};
+
+let kommuneSource = new VectorSource<KommuneFeatures>({
+  url: "./kommuner.json",
+  format: new GeoJSON(),
+});
 const kommuneLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "./kommuner.json",
-    format: new GeoJSON(),
-  }),
+  source: kommuneSource,
 });
 
 export function KommuneLayerCheckbox({
+  map,
   setLayers,
 }: {
+  map: Map;
   setLayers: Dispatch<SetStateAction<Layer[]>>;
 }) {
   const [checked, setChecked] = useState(false);
 
+  function handleClick(e: MapBrowserEvent<MouseEvent>) {
+    const clickedKommune = kommuneSource.getFeaturesAtCoordinate(e.coordinate);
+
+    if (clickedKommune.length === 1) {
+      const properties = clickedKommune[0].getProperties();
+
+      // @ts-ignore
+      alert(properties.navn.find((n) => n.sprak === "nor").navn);
+    }
+  }
+
   useEffect(() => {
     if (checked) {
       setLayers((old) => [...old, kommuneLayer]);
+      map.on("click", handleClick);
     }
     return () => {
+      map.un("click", handleClick);
       setLayers((old) => old.filter((l) => l != kommuneLayer));
     };
   }, [checked]);
